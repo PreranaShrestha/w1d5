a6adbec2dabe16239a585592a5637c8c3c56eacd
Prerana Shrestha <Prerana@Preranas-MacBook-Pro.local>
2018-01-21 08:33:54 -0800

    Add function

--- poppingBottles.js
+++ poppingBottles.js
@@ -1,31 +1,40 @@
 var poppingBottles = function(investment) {
   var bottles = Math.floor(investment / 2);
   var bottlesObj = {
-    capsEarned: 0,
-    bottlesEarned: 0
-  };
-  var totalBottles = bottles;
-  var caps = bottles;
-    while(bottles >= 2 || caps >= 4) {
-      var temp = caps;
-      caps = Math.floor(caps / 4);
-      totalBottles += caps;
-      bottles += caps;
-      bottlesObj.capsEarned += caps;
-      caps = temp % 4 + caps;
-      var temp = bottles;
-      bottles = Math.floor(bottles / 2);
-      totalBottles += bottles;
-      caps += bottles;
-      bottlesObj.bottlesEarned += bottles;
-      bottles = temp % 2 + bottles;
+    earned: {
+      caps: 0,
+      bottles: 0
+    },
+    totalBottles: bottles,
+    remaining: {
+      bottles: bottles,
+      caps: bottles
     }
-    bottlesObj.totalBottles = totalBottles;
-    bottlesObj.remainingBottles = bottles;
-    bottlesObj.remainingCaps = caps;
+  };
+  while(bottlesObj.remaining.caps >= 4 || bottlesObj.remaining.bottles >= 2) {
 
+    reedeme(bottlesObj, "caps", 4);
+    reedeme(bottlesObj, "bottles", 2);
+
+  }
   return(bottlesObj);
 };
 console.log(poppingBottles(process.argv[2]));
+  function reedeme(bottlesInfo, reedemeFrom, value) {
+    console.log(bottlesInfo, reedemeFrom, value);
+    var temp = bottlesInfo.remaining[reedemeFrom];
+    var reedemeValue = Math.floor(bottlesInfo.remaining[reedemeFrom] / value);
+    bottlesInfo.totalBottles += reedemeValue;
+    bottlesInfo.earned[reedemeFrom] += reedemeValue;
 
+  for (var key in bottlesInfo.remaining) {
+    if(key === reedemeFrom) {
+      bottlesInfo.remaining[key] = temp % value + reedemeValue;
+    } else {
+      bottlesInfo.remaining[key] += reedemeValue;
+    }
+  }
+
+ return bottlesInfo;
+  }
 
